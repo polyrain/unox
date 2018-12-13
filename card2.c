c5951baa11b0d448bed4799c2dd4abe1065aead8
s4477805 <s4477805@moss.labs.eait.uq.edu.au>
2018-12-13 22:15:33 +1000

    Updated card2.c to better reflect end design of client; still not working as output card not updated to relfect design changes. Will be fixed soon.

--- card2.c
+++ card2.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BORDER 3
+enum CardColors {
+    BLUE_BG = 1,
+    BLUE_WORDS = 2,
+    BORDER = 3,
+    RED_WORDS = 4,
+    RED_BG = 5,
+    YELLOW_BG = 6,
+    YELLOW_WORDS = 7,
+    GREEN_BG = 8,
+    GREEN_WORDS = 9
+};
+
+typedef struct Game {
+    Player* players; // All players in this game
+    Card lastPlayed; // What was last played by a player?
+    int lastPlayer; // Who played the last card?
+}Game;
+
 
 typedef struct Player {
-    char** hand; // The current hand of cards
+    Card* hand; // The current hand of cards
     int numCards; // How many do we have?
 }Player;
 
@@ -31,26 +48,31 @@ void output_display(Player* player, char* cardPlayed);
 
 
 int main() {
+    setup_ncurse();
+    output_display(NULL, NULL);
+    refresh();
+    getch();
+    endwin();
+
+    return 0;
+}
+
+void setup_ncurse(void) {
     initscr();
     raw();
     noecho();
     keypad(stdscr, TRUE);
     start_color();
     curs_set(0);
-    //getmaxyx(stdscr, row,col);
-    //mvprintw(row/2, (col-strlen(message))/2, "%s", message);
-    //mvprintw(row-2, 0, "This screen has %d rows and %d cols\n", row, col);
-    output_display(NULL, NULL);
-    //printw("Try it again after resizing!\n");
-    refresh();
-    //char c = getch();
-    //attron(A_BOLD);
-    //printw(" Key pressed was: %c\n", c);
-    //attroff(A_BOLD);
-    getch();
-    endwin();
-
-    return 0;
+    init_pair(BLUE_BG, COLOR_BLUE, COLOR_BLUE); // Setup color attrs 
+    init_pair(BLUE_WORDS, COLOR_WHITE, COLOR_BLUE); 
+    init_pair(BORDER, COLOR_WHITE, COLOR_WHITE);
+    init_pair(RED_WORDS, COLOR_WHITE, COLOR_RED);
+    init_pair(RED_BG, COLOR_RED, COLOR_RED);
+    init_pair(YELLOW_BG, COLOR_YELLOW, COLOR_YELLOW);
+    init_pair(YELLOW_WORDS, COLOR_WHITE, COLOR_YELLOW);
+    init_pair(GREEN_BG, COLOR_GREEN, COLOR_GREEN);
+    init_pair(GREEN_WORDS, COLOR_WHITE, COLOR_GREEN);
 }
 
 /*
@@ -58,19 +80,14 @@ int main() {
  * as the players current hand. Gets the terminal 
  * size and ensures drawing is done to the best 
  * standard*/
-void output_display(Player* player, char* cardPlayed) {
+void output_display(Player* player) {
     int row, col;
     getmaxyx(stdscr, row,col); // Move all these inits into another func
-    init_pair(1, COLOR_BLUE, COLOR_BLUE); // Make an entire red thing
-    init_pair(2, COLOR_WHITE, COLOR_BLUE); // Turn this into something else 
-    init_pair(BORDER, COLOR_WHITE, COLOR_WHITE);
-    init_pair(4, COLOR_WHITE, COLOR_RED);
-    init_pair(5, COLOR_RED, COLOR_RED);
-
+    
     int maxRow = row; // WHY
     row -= 15; // Put this in the middle top and fix this!!
     output_card(cardPlayed, row/2, (col - 43)/2, 4, 5);
-    output_card(cardPlayed, maxRow - 6, 20, 2, 1);
+    output_card(cardPlayed, maxRow - 6, 20, 2, 1); // Fix this function
     output_card(cardPlayed, maxRow - 6, 35, 2, 1);
     output_card(cardPlayed, maxRow - 6, 50, 2, 1);
     output_card(cardPlayed, maxRow - 6, 65, 2, 1);
