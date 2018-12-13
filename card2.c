ece55f81eae4faaace27554d3023247ff027c598
s4477805 <s4477805@moss.labs.eait.uq.edu.au>
2018-12-13 21:48:02 +1000

    First proper commit; this version DOES NOT WORK and is in the process of a re-write in order to enable proper drawing support of the system. Lost work due to not using git sooner :(

---
+++ card2.c
@@ -0,0 +1,154 @@
+#include <ncurses.h>
+#include <stdio.h>
+#include <string.h>
+
+#define BORDER 3
+
+typedef struct Player {
+    char** hand; // The current hand of cards
+    int numCards; // How many do we have?
+}Player;
+
+/* Contains information describing a card */
+typedef struct Card {
+    char* cardType;
+    int colorOne;
+    int colorTwo;
+    int cardValue;
+}Card;
+
+/* ALL CARD TYPES NOTE FOR ME
+ * DRAW FOUR: Two lines for words
+ * REVERSE: One line 
+ * DRAW TWO: Two lines
+ * NUMBER CARD: One line, numbers in corner?
+ * SKIP: One line
+ * WILD: One line
+ */
+
+void output_card(char* cardPlayed, int row, int col, int color, int color2);
+void output_display(Player* player, char* cardPlayed);
+
+
+int main() {
+    initscr();
+    raw();
+    noecho();
+    keypad(stdscr, TRUE);
+    start_color();
+    curs_set(0);
+    //getmaxyx(stdscr, row,col);
+    //mvprintw(row/2, (col-strlen(message))/2, "%s", message);
+    //mvprintw(row-2, 0, "This screen has %d rows and %d cols\n", row, col);
+    output_display(NULL, NULL);
+    //printw("Try it again after resizing!\n");
+    refresh();
+    //char c = getch();
+    //attron(A_BOLD);
+    //printw(" Key pressed was: %c\n", c);
+    //attroff(A_BOLD);
+    getch();
+    endwin();
+
+    return 0;
+}
+
+/*
+ * Draws on the screen the card last played, as well
+ * as the players current hand. Gets the terminal 
+ * size and ensures drawing is done to the best 
+ * standard*/
+void output_display(Player* player, char* cardPlayed) {
+    int row, col;
+    getmaxyx(stdscr, row,col); // Move all these inits into another func
+    init_pair(1, COLOR_BLUE, COLOR_BLUE); // Make an entire red thing
+    init_pair(2, COLOR_WHITE, COLOR_BLUE); // Turn this into something else 
+    init_pair(BORDER, COLOR_WHITE, COLOR_WHITE);
+    init_pair(4, COLOR_WHITE, COLOR_RED);
+    init_pair(5, COLOR_RED, COLOR_RED);
+
+    int maxRow = row; // WHY
+    row -= 15; // Put this in the middle top and fix this!!
+    output_card(cardPlayed, row/2, (col - 43)/2, 4, 5);
+    output_card(cardPlayed, maxRow - 6, 20, 2, 1);
+    output_card(cardPlayed, maxRow - 6, 35, 2, 1);
+    output_card(cardPlayed, maxRow - 6, 50, 2, 1);
+    output_card(cardPlayed, maxRow - 6, 65, 2, 1);
+    //ioutput_hand(player, row, col);
+}
+
+void draw_card_space(int* row, int* col, int* rowUpdate, int color, int num) {
+
+    for (int i = 0; i < num; i++) {
+        attron(COLOR_PAIR(BORDER));
+        mvprintw((*row)-(*rowUpdate)--, *col , "|");
+        attroff(COLOR_PAIR(BORDER));
+        attron(COLOR_PAIR(color));
+        printw("----------");
+        attroff(COLOR_PAIR(color));
+        attron(COLOR_PAIR(BORDER));
+        printw("|");
+    }
+}
+
+void draw_card_word(Card card) { //11 as the first char has been drawn
+    for (int i = 0; i < ((11 - strlen(card.cardPlayed))/2); i++) { 
+        printw("-");
+    }
+
+    attroff(COLOR_PAIR(color2));
+    attron(COLOR_PAIR(color));
+    attron(A_BOLD);
+    printw("DRAW");
+    attroff(A_BOLD);
+    attroff(COLOR_PAIR(color));
+    attron(COLOR_PAIR(color2));
+    printw("---");
+    attroff(COLOR_PAIR(color2));
+    attron(COLOR_PAIR(BORDER));
+    printw("|");
+    mvprintw((row)-rowUpdate--, col, "|");
+    attroff(COLOR_PAIR(BORDER));
+    attron(COLOR_PAIR(color2));
+    printw("---");
+    attroff(COLOR_PAIR(color2));
+    attron(COLOR_PAIR(color));
+    attron(A_BOLD);
+    printw("FOUR");
+    attroff(A_BOLD);
+    attroff(COLOR_PAIR(color));
+    attron(COLOR_PAIR(color2));
+    printw("---");
+    attroff(COLOR_PAIR(color2));
+    attron(COLOR_PAIR(BORDER));
+    printw("|");
+    attroff(COLOR_PAIR(BORDER));
+
+}
+
+void output_card(char* cardPlayed, int row, int col, int color, int color2) {
+    int rowUpdate = 5;
+    attron(COLOR_PAIR(BORDER));
+    mvprintw((row)-rowUpdate--, col, "+----------+");
+    draw_card_space(&row, &col, &rowUpdate, color2, 2);
+
+    attron(COLOR_PAIR(BORDER));
+    mvprintw((row)-rowUpdate--, col, "|");
+    attroff(COLOR_PAIR(BORDER));
+    attron(COLOR_PAIR(color2));
+
+        
+    draw_card_space(&row, &col, &rowUpdate, color2, 5);
+    
+    attron(COLOR_PAIR(BORDER));
+    mvprintw((row)-rowUpdate--, col, "+----------+");
+    attroff(COLOR_PAIR(BORDER));
+
+}
+
+void output_hand(Player* player, int row, int col) {
+    init_pair(1, COLOR_WHITE, COLOR_WHITE);
+    init_pair(2, COLOR_WHITE, COLOR_BLUE);
+
+
+}
